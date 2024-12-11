4c7702fefc42077a3af656107bb691db9e5b4968
Victoria Fonteny <victoriafonteny@live.fr>
2024-12-11 12:14:14 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Hello from "./composants/hello.tsx";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>hello world</h1>
+      <Hello />
+    </div>
+  );
+}
---
+++ src/composants/hello.tsx
@@ -0,0 +1,7 @@
+export default function Hello() {
+  return (
+    <div>
+      <h1>hello world, i'm victoria</h1>
+    </div>
+  );
+}
