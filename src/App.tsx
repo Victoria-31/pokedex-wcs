9adc9ea26ac8f095453372900ffe55f344356bee
Victoria Fonteny <victoriafonteny@live.fr>
2024-12-12 17:00:04 +0100

    state1

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import "./App.css";
 export default function App() {
   return (
     <div>
-      <h1>cc</h1>
+      <h1>Pokedex</h1>
       <PokemonMain />
       <Footer />
     </div>
--- src/components/PokemonsMain.tsx
+++ src/components/PokemonsMain.tsx
@@ -1,4 +1,5 @@
 import PokemonCard from "./PokemonCard";
+import { useState } from "react";
 
 const pokemonList = [
   {
@@ -7,18 +8,68 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
-    name: "mew",
+    name: "charmander",
     imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "mew",
   },
 ];
 
 export default function PokemonMain() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+  const handleClick = () => {
+    setPokemonIndex(pokemonIndex + 1);
+  };
+  const handleClickDecrement = () => {
+    setPokemonIndex(pokemonIndex - 1);
+  };
   return (
-    <div>
-      {pokemonList.map((pokemon) => (
-        <PokemonCard key={pokemon.name} pokemon={pokemon} />
-      ))}
-    </div>
+    <>
+      {/* <div>
+        {pokemonList[pokemonIndex].map((pokemon) => (
+          <PokemonCard key={pokemon.name} pokemon={pokemon} />
+        ))}
+      </div> */}
+
+      <div>
+        <PokemonCard key={pokemonIndex} pokemon={pokemonList[pokemonIndex]} />
+      </div>
+
+      {pokemonIndex > 0 ? (
+        <button onClick={handleClickDecrement} type="button">
+          {" "}
+          Précédent{" "}
+        </button>
+      ) : (
+        <p> </p>
+      )}
+
+      {pokemonIndex < pokemonList.length - 1 ? (
+        <button onClick={handleClick} type="button">
+          {" "}
+          Suivant{" "}
+        </button>
+      ) : (
+        <p> </p>
+      )}
+
+      {/* <button onClick={handleClickDecrement} type="button"> Précédent </button> */}
+      {/* <button onClick={handleClick} type="button">
+        {" "}
+        Suivant{" "}
+      </button> */}
+    </>
   );
 }
